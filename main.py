c90f3c1be8e3d3c7594e6a2c5d0a9d19c2696e74
manikanta <manikanta.basha@gmail.com>
2023-06-07 19:09:34 +0530

    commiting main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello w0rld")
+def fun(a,b):
+    sum=a+b
+    sub=a-b
+    return sum , sub
+result1,result2 =fun(100,20)
+print(result1)
+print(result2)
+
+result1,result2=fun(400,50)
+print(result1)
+print(result2)
